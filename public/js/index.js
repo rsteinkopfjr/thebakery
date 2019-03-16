0e28edd8149a6802f34eaa4acf7f33ca2ac1846e
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-03-16 02:20:15 -0400

    Clean up dashboard side

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,20 @@ var $exampleCategory = $("#example-category");
 var $exampleDescription = $("#example-description");
 var $examplePrice = $("#example-price");
 var $submitBtn = $("#submit");
+var $completeBtn = $("#completed");
 var $exampleList = $("#example-list");
+var $orderName = $("#order-name");
+var $orderEmail = $("#order-email");
+var $orderPhone = $("#order-phone");
+var $orderProduct = $("#order-product");
+var $orderQuantity = $("#order-quantity");
+var $placeOrderBtn = $("#placeOrder");
+var $addProductBtn = $("#addProduct");
+var $orderArea = $("#order-area");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -18,13 +27,13 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -33,17 +42,17 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $name = $("<h5>")
         .text("Name: " + example.text);
-        var $category = $("<h6>")
-          .text("Category: " + example.category);
-          var $description = $("<h6>")
-            .text("Description: " + example.description);
-            var $price = $("<h6>")
-              .text("Price: " + example.price);
+      var $category = $("<h6>")
+        .text("Category: " + example.category);
+      var $description = $("<h6>")
+        .text("Description: " + example.description);
+      var $price = $("<h6>")
+        .text("Price: " + example.price);
 
       var $li = $("<li>")
         .attr({
@@ -68,7 +77,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var example = {
@@ -83,7 +92,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -93,14 +102,77 @@ var handleFormSubmit = function(event) {
   $examplePrice.val("");
 };
 
+//Adding an item to the shopping cart
+var handlePlaceOrder = function (event) {
+    event.preventDefault();
+    var date = new Date();
+    var timestamp = date.toLocaleString();
+
+    var itemValue = {
+      name: $orderName.val().trim(),
+      email: $orderEmail.val().trim(),
+      phone: $orderPhone.val().trim(),
+      product: $orderProduct.val().trim(),
+      quantity: $orderQuantity.val().trim(),
+      timestamp: timestamp
+    }
+    // console.log(itemValue)
+
+    if (itemValue.product === "undefined" || (!(itemValue.quantity && itemValue.name && itemValue.email && itemValue.phone))) {
+      alert("You must fill in all forms!");
+      return;
+    }
+    var $name = $("<h5>")
+      .text("Name: " + itemValue.name);
+    var $email = $("<h6>")
+      .text("Email: " + itemValue.email);
+    var $phone = $("<h6>")
+      .text("Phone: " + itemValue.phone);
+    var $product = $("<h6>")
+      .text("Item: " + itemValue.product);
+    var $quantity = $("<h6>")
+      .text("Quantity: " + itemValue.quantity);
+    var $timestamp = $("<h6>")
+      .text("Time of Order: " + itemValue.timestamp);
+    var $button = $("<button>")
+      .addClass("btn btn-danger float-left delete")
+      .text("Delete Product");
+
+    var $order = $("<li>");
+    $order.addClass("list-group-item");
+    $order.append($name, $email, $phone, $product, $quantity, $timestamp);
+    $order.append($button);
+
+
+    $orderArea.append($order);
+    $orderName.val("");
+    $orderEmail.val("");
+    $orderPhone.val("");
+    $orderProduct.val("undefined");
+    $orderQuantity.val("");
+};
+
+var handleCompleteOrder = function () {
+  console.log(this);
+}
+//Figure out in version 2
+// var handleAddProductForm = function (event) {
+//   event.preventDefault();
+//   console.log(this);
+//   var formItem = document.getElementById("product-order-area");
+//   var cln = formItem.cloneNode(true);
+//   document.getElementById("product-order-area-two").append(cln);
+// };
+
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
+  console.log(this);
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
@@ -108,3 +180,4 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+$placeOrderBtn.on("click", handlePlaceOrder);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,45 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
-        msg: "Bakery Dashbord",
+        msg: "Bakery Website",
         examples: dbExamples
       });
     });
   });
   // Load products page
-  app.get("/products", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/products", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("products", {
+        msgProd: "Bakery Dashbord",
+        examples: dbExamples
+      });
+    });
+  });
+  // Load orders page
+  app.get("/orders", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("orders", {
+        msgOrd: "Bakery Dashbord",
+        examples: dbExamples
+      });
+    });
+  });
+  // Load order form page
+  app.get("/orderform", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("orderForm", {
         examples: dbExamples
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
       res.render("example", {
@@ -31,7 +49,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
