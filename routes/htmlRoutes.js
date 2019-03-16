4c8e9fb4235c6240f17ba28da7ef9315ded768e3
Nikolinka Cieloszczyk <cnikolinka@gmail.com>
2019-03-16 12:06:56 -0400

    changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,15 +20,52 @@ module.exports = function(app) {
       });
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/about-us", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("about-us", {
+        title: "about-us",
+        msg: "Welcome!",
+        examples: dbExamples
       });
     });
   });
+  app.get("/check-out", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("check-out", {
+        title: "check-out",
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/contact-us", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("contact-us", {
+        title: "contact-us",
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/shop-page", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("shop-page", {
+        title: "shop-page",
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/shopping-cart", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("shopping-cart", {
+        title: "shopping-cart",
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
