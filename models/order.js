5db69f24f0e23b71314408f7f03cb3fb1ce1af7a
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-03-21 17:51:21 -0400

    Make prettier

--- models/order.js
+++ models/order.js
@@ -1,20 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
-    var Order = sequelize.define("Order", {
-        firstname: DataTypes.STRING,
-        lastname: DataTypes.STRING,
-        email: DataTypes.STRING,
-        phone: DataTypes.STRING,
-        pickupdate: DataTypes.STRING,
-        pickuptime: DataTypes.STRING,
-        product: DataTypes.STRING,
-        quantity: DataTypes.INTEGER,
-        timestamp: DataTypes.STRING,
-        completed: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        note: DataTypes.TEXT
-    });
-    return Order;
-  };
+  var Order = sequelize.define("Order", {
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    pickupdate: DataTypes.STRING,
+    pickuptime: DataTypes.STRING,
+    product: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    timestamp: DataTypes.STRING,
+    completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    note: DataTypes.TEXT
+  });
+  return Order;
+};
   
