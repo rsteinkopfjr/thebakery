733d99e1a7695bd525b006de7850e100bc714056
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-03-27 21:22:10 -0400

    Prettier

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -44,7 +44,7 @@ module.exports = function(app) {
         completed: true
       },
       { where: { id: req.params.id } }
-    ).then(function(dbOrders){
+    ).then(function(dbOrders) {
       res.json(dbOrders);
     });
   });
@@ -69,8 +69,7 @@ module.exports = function(app) {
   });
   // Deletes an order by id
   app.delete("/api/orders/:id", function(req, res) {
-    db.Order.destroy({ where: { id: req.params.id } }).then(function(dbOrders
-      ) {
+    db.Order.destroy({ where: { id: req.params.id } }).then(function(dbOrders) {
       res.json(dbOrders);
     });
   });
